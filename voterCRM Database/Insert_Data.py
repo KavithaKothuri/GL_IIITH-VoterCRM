691873434405f7976ed89c819af88de85ccd28c2
Naseem <108058258+AnwarNaseem@users.noreply.github.com>
2023-11-13 21:29:41 +0530

    Update Insert_Data.py

--- voterCRM Database/Insert_Data.py
+++ voterCRM Database/Insert_Data.py
@@ -1,21 +1,35 @@
 import csv
-from sqlconnection import connection
-from sqlconnection import database as db
+import pandas as pd
+from sqlconnection import connection, cursor
 
-db = "VOTER_DATA"  # This is the name of the database 
 
-CONFIG_PATH = "D:\CAPSTONE PROJECT\voterCRM\voterCRM sample data.xlsx"
-USERS = 'users'
+# Read data from the CSV file into a DataFrame
+csv_file = "voterCRM_sample_data.csv"
+data = pd.read_csv(csv_file)
 
-with open(CONFIG_PATH + USERS + '.csv','r') as f:
-    values = []
-    data = csv.reader(f)
-    for row in data:
-        values.append(tuple(row))
-    sql =  """
-    insert into users (user_id,user_name, user_email, user_password, user_address)
-    values ("%s,%s,%s,%s,%s,%s")
-    """
-    values.pop(0)
-    db.insert_many_records(connection,sql,values)
-print("Data insertion in User Table completed")
+# Function to insert data from a CSV file into a table
+def insert_data_from_csv(csv_filename, table_name):
+    with open(csv_filename, 'r') as csvfile:
+        csv_reader = csv.reader(csvfile)
+        next(csv_reader)  # Skip the header row if it exists in your CSV file
+        for row in csv_reader:
+            # Define your SQL INSERT statement
+            insert_query = f"INSERT INTO {table_name} VALUES ({', '.join(['%s'] * len(row))})"
+            cursor.execute(insert_query, row)
+            print("Data insertion completed")
+            
+
+# Insert data into each table
+insert_data_from_csv('voterCRM_sample_data.csv', 'USERS')
+insert_data_from_csv('voterCRM_sample_data.csv', 'VOTER')
+insert_data_from_csv('voterCRM_sample_data.csv', 'ADDRESS')
+insert_data_from_csv('voterCRM_sample_data.csv', 'CONTACT')
+insert_data_from_csv('voterCRM_sample_data.csv', 'FAMILY')
+insert_data_from_csv('voterCRM_sample_data.csv', 'RELIGION')
+insert_data_from_csv('voterCRM_sample_data.csv', 'POLITICAL_AFFILIATION')
+insert_data_from_csv('voterCRM_sample_data.csv', 'POLICE_CASE')
+
+
+# Commit the changes and close the connection
+connection.commit()
+connection.close()
